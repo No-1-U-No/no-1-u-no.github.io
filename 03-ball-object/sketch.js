1f03429dcb468c211e3cb55abb6348c2e10f9fe7
No-1-U-No <gabepj7125@outlook.com>
2023-10-06 10:38:18 -0600

    Updated code

--- 03-ball-object/sketch.js
+++ 03-ball-object/sketch.js
@@ -1,19 +1,11 @@
 // Ball Object Notation Demo
 // Oct 5, 2023
 
-let ball = {
-  x: 100,
-  y: 100,
-  d: 50,
-  r: 255,
-  g: 0,
-  b: 0,
-  dx: 10,
-  dy: -10
-};
+let ball;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ball = spawnBall();
 }
 
 function draw() {
@@ -22,17 +14,37 @@ function draw() {
   displayBall();
 }
 
+function keyTyped() {
+  if (key === " ") {
+    ball = spawnBall();
+  }
+}
+
+function spawnBall() {
+  let ball = {
+    x: random(width),
+    y: random(height),
+    d: random(30, 60),
+    r: random(255),
+    g: random(255),
+    b: random(255),
+    dx: random(-5, 5),
+    dy: random(-5, 5)
+  };
+  return ball;
+}
+
 function moveBall() {
-  if (ball.dx > 0 && ball.x >= width) {
+  if (ball.dx > 0 && ball.x - ball.d > width) {
     ball.x = 0 - ball.d;
   }
-  else if (ball.dy > 0 && ball.y >= height) {
+  else if (ball.dy > 0 && ball.y + ball.d > height) {
     ball.y = 0 - ball.d;
   }
-  else if (ball.dx < 0 && ball.x <= 0) {
+  else if (ball.dx < 0 && ball.x - ball.d < 0) {
     ball.x = width - ball.d;
   }
-  else if (ball.dy < 0 && ball.y <= 0) {
+  else if (ball.dy < 0 && ball.y + ball.d < 0) {
     ball.y = height - ball.d;
   }
   else {
