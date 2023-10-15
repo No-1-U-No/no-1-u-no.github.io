ef9765748de873b26bf8f006f989e6acb965071d
No-1-U-No <gabepj7125@outlook.com>
2023-10-15 13:33:20 -0600

    Got rid of values() and finally created objects! Likely next step: start the game itself

--- 04-arrays-objects-assignment/sketch.js
+++ 04-arrays-objects-assignment/sketch.js
@@ -16,50 +16,43 @@ let gameScreen = "Game Description";
 let patience = 10;
 let items = 0;
 
-let dad;
+let boyImage;
+let dadImage;
+
+let introScreen;
+
 let boy;
-
-let gameTitleSize;
-let gameTitleYcor;
-let gameInstructionsSize;
-let gameDescriptionYcor;
-let gameInstructionsYcor;
-let footerSize;
-let footerYcor;
-
-let dadWidth;
-let dadHeight;
-let dadXcor;
-let dadYcor;
-
-let boyWidth;
-let boyHeight;
-let boyYcor;
-let firstBoyXcor;
-let secondBoyXcor;
+let dad;
+let button;
 
 function preload() {
-  // Dad image source: https://media.istockphoto.com/id/1130554249/photo/elderly-man-in-white-shirt-with-his-arms-crossed-looking-depressed-and-annoyed.jpg?s=612x612&w=0&k=20&c=MJu2j1FeHF6vIKeiizq-enVRf6aotyRBVVNILldtZds=
   // Boy image source: https://metro.co.uk/wp-content/uploads/2015/08/child-screaming.jpg
-
-  dad = loadImage("annoyed-dad.png");
-  boy = loadImage("annoying-boy.png");
+  // Dad image source: https://media.istockphoto.com/id/1130554249/photo/elderly-man-in-white-shirt-with-his-arms-crossed-looking-depressed-and-annoyed.jpg?s=612x612&w=0&k=20&c=MJu2j1FeHF6vIKeiizq-enVRf6aotyRBVVNILldtZds=
+  
+  boyImage = loadImage("annoying-boy.png");
+  dadImage = loadImage("annoyed-dad.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  values();
+  introScreen = displayIntroScreens();
+  boy = displayBoys();
+  dad = displayDad();
+  button = displayButtons();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  values();
+  introScreen = displayIntroScreens();
+  boy = displayBoys();
+  dad = displayDad();
+  button = displayButtons();
 }
 
 function draw() {
   background(0);
   description();
-
+  
   if (gameScreen === "Game Instructions") {
     background(50);
     instructions();
@@ -71,39 +64,41 @@ function draw() {
   }
 }
 
-function values() {
-  gameTitleSize = 0.0625*width;
-  gameTitleYcor = height/77.5;
-  gameInstructionsSize = 0.03125*width;
-  gameDescriptionYcor = height/2 - 2*gameInstructionsSize;
-  gameInstructionsYcor = height/2 - 2.5*gameInstructionsSize;
-  footerSize = 0.025*width;
-  footerYcor = 0.9625*height - footerSize/2;
+function displayIntroScreens() {
+  let introScreen = {
+    titleSize: 0.0625*width,
+    titleText: "DOUBLE TROUBLE",
+    xcor: 0,
+    titleYcor: height/77.5,
+    textBoxWidth: width,
+    textBoxHeight: height,
 
-  dadWidth = 0.3825*width;
-  dadHeight = 0.525*height;
-  dadXcor = width/2;
-  dadYcor = height - dadHeight/2;
+    textSize: 0.03125*width,
+    textYcor: height/2 - 0.0625*width,
+    descriptionText: "Dad needs to go to the grocery store, but his twin boys are extremely difficult when it comes to making the list. One boy asks for too many things, while the other complains about nearly anything his brother asks for. Both behaviours frustrate Dad, and he will eventually blow up.",
+    instructionsText: "You are the boy who asks for everything. If you ask Dad for healthy food and both him and your brother are happy with it, Dad will add it to the list. If Dad rejects 10 of your requests before completing a list with 10 items on it, you and your brother will be grounded for a week.",
+    
+    footerSize: 0.025*width,
+    footerYcor: 0.9625*height - 0.0125*width,
+    descriptionFooterText: "(click anywhere on screen to continue)",
+    instructionsFooterText: "(press Enter to start game)",
+  }
 
-  boyWidth = 0.3*width;
-  boyHeight = 0.43875*height;
-  boyYcor = height - boyHeight/2;
-  firstBoyXcor = boyWidth/2;
-  secondBoyXcor = 0.85*width;
+  return introScreen;
 }
 
 function description() {
   textAlign(CENTER);
-  textSize(gameTitleSize);
+  textSize(introScreen.titleSize);
   fill("red");
-  text("DOUBLE TROUBLE", 0, gameTitleYcor, width, height);
+  text(introScreen.titleText, introScreen.xcor, introScreen.titleYcor, introScreen.textBoxWidth, introScreen.textBoxHeight);
 
-  textSize(gameInstructionsSize);
-  text("Dad needs to go to the grocery store, but his twin boys are extremely difficult when it comes to making the list. One boy asks for too many things, while the other complains about nearly anything his brother asks for. Both behaviours frustrate Dad, and he will eventually blow up.", 0, gameDescriptionYcor, width, height);
-
-  textSize(footerSize);
-  text("(click anywhere on screen to continue)", 0, footerYcor, width, height);
-
+  textSize(introScreen.textSize);
+  text(introScreen.descriptionText, introScreen.xcor, introScreen.textYcor, introScreen.textBoxWidth, introScreen.textBoxHeight);
+  
+  textSize(introScreen.footerSize);
+  text(introScreen.descriptionFooterText, introScreen.xcor, introScreen.footerYcor, introScreen.textBoxWidth, introScreen.textBoxHeight);
+  
   if (mouseIsPressed && gameScreen === "Game Description") {
     gameScreen = "Game Instructions";
   }
@@ -111,29 +106,102 @@ function description() {
 
 function instructions() {
   textAlign(CENTER);
-  textSize(gameTitleSize);
-  text("DOUBLE TROUBLE", 0, gameTitleYcor, width, height);
+  textSize(introScreen.titleSize);
+  text(introScreen.titleText, introScreen.xcor, introScreen.titleYcor, introScreen.textBoxWidth, introScreen.textBoxHeight);
+  
+  textSize(introScreen.textSize);
+  text(introScreen.instructionsText, introScreen.xcor, introScreen.textYcor, introScreen.textBoxWidth, introScreen.textBoxHeight);
 
-  textSize(gameInstructionsSize);
-  text("You are the boy who asks for everything. If you ask Dad for healthy food and both him and your brother are happy with it, Dad will add it to the list. The goal is to have a list with 15 items, but if your dad rejects 10 of your requests before then, you and your brother will be grounded for a week.", 0, gameInstructionsYcor, width, height);
-
-  textSize(footerSize);
-  text("(press Enter to start game)", 0, footerYcor, width, height);
-
+  textSize(introScreen.footerSize);
+  text(introScreen.instructionsFooterText, introScreen.xcor, introScreen.footerYcor, introScreen.textBoxWidth, introScreen.textBoxHeight);
+  
   if (keyIsPressed && keyCode === ENTER) {
     gameScreen = "Game On";
   }
 }
 
-function game() {
-  imageMode(CENTER);
-  image(boy, firstBoyXcor, boyYcor, boyWidth, boyHeight);
-  image(dad, dadXcor, dadYcor, dadWidth, dadHeight);
-  image(boy, secondBoyXcor, boyYcor, boyWidth, boyHeight);
+function displayBoys() {
+  let boy = {
+    firstBoyXcor: 0.15*width,
+    secondBoyXcor: 0.85*width,
+    ycor: height - 0.219375*height,
+    width: 0.3*width,
+    height: 0.43875*height,
+  };
 
+  return boy;
+}
+
+function displayDad() {
+  let dad = {
+    xcor: width/2,
+    ycor: height - 0.2625*height,
+    width: 0.3825*width,
+    height: 0.525*height,
+  };
+  
+  return dad;
+}
+
+function displayButtons() {
+  let button = {
+    addItemsButtonXcor: boy.firstBoyXcor + introScreen.textSize,
+    viewListButtonXcor: boy.secondBoyXcor - introScreen.textSize,
+    ycor: height/7.75,
+    width: boy.width,
+    height: height/7.75,
+
+    textSize: 1.5*introScreen.textSize,
+    textYcor: height/7.75 + height/150,
+    addItemsText: "ADD ITEMS",
+    viewListText: "VIEW LIST",
+
+    leftAddItemsText: boy.firstBoyXcor + introScreen.textSize - boy.width/2,
+    rightAddItemsText: boy.firstBoyXcor + introScreen.textSize + boy.width/2,
+    leftViewListText: boy.secondBoyXcor - introScreen.textSize - boy.width/2,
+    rightViewListText: boy.secondBoyXcor - introScreen.textSize + boy.width/2,
+    topText: height/7.75 - height/15.5,
+    bottomText: height/7.75 + height/15.5,
+  };
+
+  return button;
+}
+
+function displayGameScreen() {
+  imageMode(CENTER);
+  image(boyImage, boy.firstBoyXcor, boy.ycor, boy.width, boy.height);
+  image(dadImage, dad.xcor, dad.ycor, dad.width, dad.height);
+  image(boyImage, boy.secondBoyXcor, boy.ycor, boy.width, boy.height);
+  
+  fill(50);
   rectMode(CENTER);
-  rect(firstBoyXcor, 100, boyWidth, 100);
-  textSize(gameTitleSize);
-  text(patience, dadXcor, 100, dadWidth, 100);
-  rect(secondBoyXcor, 100, boyWidth, 100);
+  rect(button.addItemsButtonXcor, button.ycor, button.width, button.height);
+  rect(button.viewListButtonXcor, button.ycor, button.width, button.height);
+  
+  fill("green");
+  textAlign(CENTER, CENTER);
+  textSize(button.textSize);
+  text(button.addItemsText, button.addItemsButtonXcor, button.textYcor, button.width, button.height);
+  text(button.viewListText, button.viewListButtonXcor, button.textYcor, button.width, button.height);
+  
+  fill("red");
+  text(patience, dad.xcor, button.textYcor, dad.width, button.height);
+}
+
+function game() {
+  displayGameScreen();
+
+  if (mouseX >= button.leftAddItemsText && mouseX <= button.rightAddItemsText && mouseY >= button.topText && mouseY <= button.bottomText) {
+    fill("green");
+    rect(button.addItemsButtonXcor, button.ycor, button.width, button.height);
+    fill(50);
+    text(button.addItemsText, button.addItemsButtonXcor, button.textYcor);
+  }
+
+  if (mouseX >= button.leftViewListText && mouseX <= button.rightViewListText && mouseY >= button.topText && mouseY <= button.bottomText) {
+    fill("green");
+    rect(button.viewListButtonXcor, button.ycor, button.width, button.height);
+    fill(50);
+    text(button.viewListText, button.viewListButtonXcor, button.textYcor);
+  }
 }
