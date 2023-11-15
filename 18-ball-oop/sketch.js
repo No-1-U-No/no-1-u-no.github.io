eda468c27a72b2142c6eaa10e2bdd5cfe142892c
No-1-U-No <gabepj7125@outlook.com>
2023-11-15 12:16:53 -0600

    Ball OOP Demo

--- 18-ball-oop/sketch.js
+++ 18-ball-oop/sketch.js
@@ -1,15 +1,79 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Ball OOP Demo
 
+class Ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = random(15, 30);
+    this.dx = random(-5, 5);
+    this.dy = random(-5, 5);
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+  }
+
+  move() {
+    if (this.x - this.radius <= 0 || this.x + this.radius >= width) {
+      this.dx *= -1;
+    }
+
+    else if (this.y - this.radius < 0 || this.y + this.radius > height) {
+      this.dy *= -1;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  display() {
+    fill(this.r, this.g, this.b);
+    circle(this.x, this.y, 2*this.radius);
+  }
+
+  bounceOff(otherBall) {
+    let radiiSum = this.radius + otherBall.radius;
+    let distanceApart = dist(this.x, this.y, otherBall.x, otherBall.y);
+
+    if (radiiSum > distanceApart) {
+      //balls hitting each other
+      let tempX = this.dx;
+      let tempY = this.dy;
+
+      this.dx = otherBall.dx;
+      this.dy = otherBall.dy;
+
+      otherBall.dx = tempX;
+      otherBall.dy = tempY;
+    }
+  }
+}
+
+let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  let ball = new Ball(width/2, height/2);
+  ballArray.push(ball);
 }
 
 function draw() {
   background(220);
+  for (let currentBall of ballArray) {
+    currentBall.move();
+
+    for (let otherBall of ballArray) {
+      //avoid checking if hitting self
+      if (currentBall !== otherBall) {
+        currentBall.bounceOff(otherBall);
+      }
+    }
+
+    currentBall.display();
+  }
+}
+
+function mousePressed() {
+  let ball = new Ball(mouseX, mouseY);
+  ballArray.push(ball);
 }
