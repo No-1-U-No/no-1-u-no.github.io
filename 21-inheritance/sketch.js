14d4873c987ad61e3af312889c8587428dc8be90
No-1-U-No <gabepj7125@outlook.com>
2023-11-24 11:36:11 -0600

    Finished demo

--- 21-inheritance/sketch.js
+++ 21-inheritance/sketch.js
@@ -1,23 +1,27 @@
 // Inheritance OOP Demo
 
 let particle;
+let confetti;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   particle = new Particle(width/2, height/2);
+  confetti = new Confetti (width/2, height/2);
 }
 
 function draw() {
-  background(220);
+  background(0);
   particle.update();
   particle.display();
+  confetti.update();
+  confetti.display();
 }
 
 class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 10;
+    this.size = 50;
   }
 
   update() {
@@ -33,5 +37,14 @@ class Particle {
 class Confetti extends Particle {
   constructor(x, y) {
     super(x, y);
+  }
+
+  update() {
+    super.update();
+    this.size += random(-5, 5);
+  }
+
+  display() {
+    square(this.x, this.y, this.size);
   }
 }
